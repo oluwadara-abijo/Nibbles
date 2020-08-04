2e5e74353f02e8179d9bc31ba12a0cc7b9887e9b
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2020-08-04 13:08:49 +0100

    feat: Create home screen

---
+++ app/src/main/java/com/dara/nibbles/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.dara.nibbles
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+/**
+ * A simple [Fragment] subclass.
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
